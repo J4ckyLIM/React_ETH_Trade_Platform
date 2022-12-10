dcf29f68bb075661e9144cb83bbeb1c4ff9c273a
LIM Jacky <42095148+J4ckyLIM@users.noreply.github.com>
2022-12-10 12:09:19 +0100

    feat: Notification system (#3)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,14 +6,29 @@ import {
 import { Suspense } from "react";
 import { useRoutes } from "react-router-dom";
 import { routes } from "./router";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   const elements = useRoutes(routes);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Suspense>{elements}</Suspense>
       </Box>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </ChakraProvider>
   )
 }
