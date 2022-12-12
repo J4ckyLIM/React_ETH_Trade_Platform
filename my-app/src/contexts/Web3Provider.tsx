f39cba39833360e0a9cf736c9c70215458131983
Jacky LIM <j4cky.lm@gmail.com>
2022-12-13 00:34:29 +0100

    feat: web3 provider

---
+++ my-app/src/contexts/Web3Provider.tsx
@@ -0,0 +1,30 @@
+import { createContext, useMemo, useState } from "react"
+
+import { UserWallet } from "../types/types";
+
+interface Web3Config {
+  wallet: UserWallet | null;
+  setWallet: (wallet: UserWallet | null) => void;
+};
+
+export const Web3Context = createContext<Web3Config>({
+  wallet: null,
+  setWallet: () => {},
+});
+
+export const Web3Provider = ({ children }: { children: any }) => {
+  const [wallet, setWallet] = useState<UserWallet | null>(null);
+
+  const contextValues = useMemo(() => {
+    return {
+      wallet,
+      setWallet
+    };
+  }, [wallet]);
+
+  return (
+    <Web3Context.Provider value={contextValues}>
+      {children}
+    </Web3Context.Provider>
+  )
+}
--- my-app/src/contexts/index.ts
+++ my-app/src/contexts/index.ts
@@ -1,1 +1,2 @@
 export * from './AuthenticationProvider';
+export * from './Web3Provider';
