baf2e8ab89f6ead8b67104a15f0dd946f1ccd1a0
LIM Jacky <42095148+J4ckyLIM@users.noreply.github.com>
2022-12-17 23:21:32 +0100

    feat: remove (#10)

--- my-app/src/config/firebase/firebase.ts
+++ my-app/src/config/firebase/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import { getAuth } from 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDlhhdAhp-u8Z5knDFzsqcd-SOM4S-FoO8",
+  apiKey: "xxx",
   authDomain: "react-eth-swap.firebaseapp.com",
   databaseURL: "https://react-eth-swap-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "react-eth-swap",
   storageBucket: "react-eth-swap.appspot.com",
-  messagingSenderId: "248034315420",
-  appId: "1:248034315420:web:d43bc0b2dcffc3641a3159"
+  messagingSenderId: "xxx",
+  appId: "xxx"
 }
 
 const app = firebase.initializeApp(config);
